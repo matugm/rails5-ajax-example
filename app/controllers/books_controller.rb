c97554b09a726547f9bab8e544148ad9b5b5c26a
Jesus Castello <matugm@gmail.com>
2019-04-08 19:52:35 +0200

    Implement random AJAX book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,10 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def random_book
+    @book = Book.all.sample
+  end
+
   # GET /books/1
   # GET /books/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books do
+    get :random, to: 'books#random_book', on: :collection
+  end
 end
